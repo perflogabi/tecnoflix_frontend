5b771ed5985d6212f9d272de726aa8d0c6cbd487
Gabriela Florêncio <perflogabi@gmail.com>
2023-12-10 21:59:30 -0500

    Criação dos elementos, do visual e também da sua responsividade - Seção de apresentação, homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss"
 import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
+import PresentationSection from "@/components/presentationSection";
 
 const HomeNotAuth = function () {
   return (
 		<>
 			<Head>
         <title>Techflix</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         <meta property="og:title" content="Techtflix" key="title" />
 				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
       </Head>
 			<main>
-        <HeaderNoAuth />
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
       </main>
 		</>
   )
--- src/components/homeNoAuth/headerNoAuth/index.tsx
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -5,18 +5,23 @@ import Link from "next/link";
 const HeaderNoAuth = function () {
     return <>
         <div className={styles.ctaSection}>
-            <img src="/homeNotAuth/logoCta.png" alt="logoCta" className={styles.imgCta} />
+            <img src="/icone.png" alt="logoCta" className={styles.imgCta} />
             <p>Se cadastre para ter acesso aos cursos</p>
-            <img src="/homeNotAuth/logoCta.png" alt="logoCta" className={styles.imgCta} />
+            <img src="/icone.png" alt="logoCta" className={styles.imgCta} />
         </div>
         <Container className={styles.nav}>
-            <img src="/logo2.png" alt="logotechflix" className={styles.imgLogoNav} />
-            <Link href="/login">
-                <Button className={styles.navBtn} outline>Entrar</Button>
-            </Link>
-            <Link href="/register">
-                <Button className={styles.navBtn} outline>Quero fazer parte</Button>
-            </Link>
+            <div className="logo">
+                <img src="/logo2.png" alt="logotechflix" className={styles.imgLogoNav} />
+            </div>
+            <div className="btn">
+                <Link href="/login">
+                    <Button className={styles.navBtn} outline>Entrar</Button>
+                </Link>
+                <Link href="/register">
+                    <Button className={styles.navBtn} outline>Quero fazer parte</Button>
+                </Link>
+            </div>
+
         </Container>
     </>;
 };
