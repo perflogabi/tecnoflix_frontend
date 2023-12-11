95e17ab7630d7c39366354b23de4700be9a8fb1a
Gabriela Florêncio <perflogabi@gmail.com>
2023-12-10 22:29:47 -0500

    Seção de cards, homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss"
 import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "@/components/presentationSection";
+import CardsSection from "@/components/homeNoAuth/cardsSection";
 
 const HomeNotAuth = function () {
   return (
@@ -17,6 +18,7 @@ const HomeNotAuth = function () {
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <CardsSection />
       </main>
 		</>
   )
