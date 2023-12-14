22330a94ae50662d4cd22745ac657ea7ca774b6b
Gabriela Florêncio <perflogabi@gmail.com>
2023-12-14 14:27:20 -0500

    página search

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,6 +4,9 @@ import HeaderAuth from "../src/components/commom/headerAuth";
 import { useRouter } from "next/router";
 import courseService, { CourseType } from "@/services/courseService";
 import { useEffect, useState } from "react";
+import { Container } from "reactstrap";
+import SearchCard from "@/components/searchCArd";
+import Footer from "@/components/commom/footer";
 
 const Search = function () {
     const router = useRouter();
@@ -35,13 +38,24 @@ const Search = function () {
                 <title>Techtflix - {searchName}</title>
                 <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
             </Head>
-            <main>
-                <HeaderAuth />
-                {searchResult?.map((course) => (
-                    <div key={course.id}>
-                        <p>{course.name}</p>
+            <main className={styles.main}>
+                <div className={styles.header}>
+                    <HeaderAuth />
+                </div>
+                {searchResult.length >= 1 ? (
+                    <div className={styles.searchResult}>
+                        <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+                            {searchResult?.map((course) => (
+                                <SearchCard key={course.id} course={course} />
+                            ))}
+                        </Container>
                     </div>
-                ))}
+                ) : (
+                    <p className={styles.noSearchText}>Nenhum resultado encontrado!</p>
+                )}
+                <div className={styles.footer}>
+                    <Footer />
+                </div>
             </main>
         </>
     );
